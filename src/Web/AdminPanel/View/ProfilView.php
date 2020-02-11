8ad359c480de146a5da35e7dabb10a5582da7acc
Marcys <moovspace@github.com>
2020-02-11 22:39:01 +0100

    PhpApix sample app

--- src/Web/AdminPanel/View/ProfilView.php
+++ src/Web/AdminPanel/View/ProfilView.php
@@ -7,6 +7,7 @@ use MyApp\Web\AdminPanel\LeftMenu;
 use MyApp\Web\AdminPanel\User;
 use MyApp\App\Menu\Menu;
 use MyApp\Web\AdminPanel\TopMenu;
+use MyApp\App\Translate\Trans;
 
 class ProfilView extends Component
 {
@@ -140,6 +141,8 @@ class ProfilView extends Component
 
 	static function Show($arr = null)
 	{
+		$t = new Trans('/src/Web/AdminPanel/Lang', 'pl');
+
 		// Get data
 		$user = self::Data();
 
@@ -147,19 +150,20 @@ class ProfilView extends Component
 		$arr['user'] = $user->GetUser();
 		$arr['user_info'] = $user->GetUserInfo();
 		$arr['error'] = '';
+		$arr['trans'] = $t;
 
 		if(!empty($_POST))
 		{
 			if($user->ErrorUpdate == 0){
-				$arr['error'] = '<span class="green">Nothing to update.</span>';
+				$arr['error'] = '<span class="green"> '.$t->Get('ERR_NOTHING').' </span>';
 			}else if($user->ErrorUpdate == 1){
-				$arr['error'] = '<span class="green">Profil has been updated.</span>';
+				$arr['error'] = '<span class="green"> '.$t->Get('UPDATED').' </span>';
 			}else if($user->ErrorUpdate == -3){
-				$arr['error'] = '<span class="red">Error image file (Only .jpeg, .jpg files).</span>';
+				$arr['error'] = '<span class="red"> '.$t->Get('ERR_IMAGE').' </span>';
 			}else if($user->ErrorUpdate == -2){
-				$arr['error'] = '<span class="red">This username is aledy taken.</span>';
+				$arr['error'] = '<span class="red"> '.$t->Get('ERR_USERNAME').' </span>';
 			}else if($user->ErrorUpdate < 0){
-				$arr['error'] = '<span class="red">Can not update profil.</span>';
+				$arr['error'] = '<span class="red"> '.$t->Get('ERR_UPDATE').' </span>';
 			}
 		}
 
@@ -178,88 +182,88 @@ class ProfilView extends Component
 		<div id="box">
 			'.$html['left'].'
 			<div id="box-right">
-				<h1>Profil settings</h1>
+				<h1> '.$arr['trans']->Get('PR_SETTINGS').' </h1>
 				<error id="error">
 					' . $arr['error'] . '
 				</error>
 				<div class="box-wrap">
-					<h3>Account</h3>
+					<h3> '.$arr['trans']->Get('PR_ACCOUNT').' </h3>
 					<form method="post" enctype="multipart/form-data">
 						<div class="w-50">
-							<label>Username</label>
-							<input type="text" name="username" placeholder="Username" value="' . $arr['user']['username'] . '">
+							<label> '.$arr['trans']->Get('LB_USER').' </label>
+							<input type="text" name="username" placeholder="'.$arr['trans']->Get('EG').' Username" value="' . $arr['user']['username'] . '">
 						</div>
 						<div class="w-50">
-							<label>Email <small>(disabled)</small> </label>
-							<input type="text" name="email" placeholder="your@email.address" disabled value="' . $arr['user']['email'] . '">
+							<label> '.$arr['trans']->Get('LB_EMAIL').' <small>(disabled)</small> </label>
+							<input type="text" name="email" placeholder="'.$arr['trans']->Get('EG').' your@email.address" disabled value="' . $arr['user']['email'] . '">
 						</div>
 						<div class="w-50">
-							<label>Mobile</label>
-							<input type="text" name="mobile" placeholder="+48 000 000 000" value="' . $arr['user']['mobile'] . '">
+							<label> '.$arr['trans']->Get('LB_MOBILE').' </label>
+							<input type="text" name="mobile" placeholder="'.$arr['trans']->Get('EG').' +48 000 000 000" value="' . $arr['user']['mobile'] . '">
 						</div>
 						<div class="w-50">
-							<label>Avatar (Extension: .jpg, .jpeg)</label>
-							<input type="file" name="file" placeholder="Avatar">
+							<label> '.$arr['trans']->Get('LB_AVATAR').' </label>
+							<input type="file" name="file" placeholder="'.$arr['trans']->Get('EG').' Avatar">
 						</div>
 						<line></line>
-						<input type="submit" name="account" value="Save" class="btn float-right">
+						<input type="submit" name="account" value="'.$arr['trans']->Get('SAVE').'" class="btn float-right">
 					</form>
 				</div>
 
 				<div class="box-wrap">
-					<h3>About user</h3>
+					<h3> '.$arr['trans']->Get('PR_ABOUTUSER').' </h3>
 					<form method="post">
 						<div class="w-50">
-							<label>Firstname</label>
-							<input type="text" name="firstname" placeholder="First Name" value="' . $arr['user_info']['firstname'] . '">
+							<label> '.$arr['trans']->Get('LB_FIRST').' </label>
+							<input type="text" name="firstname" placeholder="'.$arr['trans']->Get('EG').' Alicja" value="' . $arr['user_info']['firstname'] . '">
 						</div>
 						<div class="w-50">
-							<label>Lastname</label>
-							<input type="text" name="lastname" placeholder="Last Name" value="' . $arr['user_info']['lastname'] . '">
+							<label> '.$arr['trans']->Get('LB_LAST').' </label>
+							<input type="text" name="lastname" placeholder="'.$arr['trans']->Get('EG').' Piekna" value="' . $arr['user_info']['lastname'] . '">
 						</div>
 						<div class="w-50">
-							<label>Email</label>
-							<input type="text" name="mail" placeholder="your@email.address" value="' . $arr['user_info']['mail'] . '">
+							<label> '.$arr['trans']->Get('LB_EMAIL').' </label>
+							<input type="text" name="mail" placeholder="'.$arr['trans']->Get('EG').' your@email.address" value="' . $arr['user_info']['mail'] . '">
 						</div>
 						<div class="w-50">
-							<label>Mobile</label>
-							<input type="text" name="mobile" placeholder="+48 000 000 000" value="' . $arr['user_info']['mobile'] . '">
+							<label> '.$arr['trans']->Get('LB_MOBILE').' </label>
+							<input type="text" name="mobile" placeholder="'.$arr['trans']->Get('EG').' +48 000 000 000" value="' . $arr['user_info']['mobile'] . '">
 						</div>
 						<div class="w-50">
-							<label>Longtitude</label>
-							<input type="text" name="lng" placeholder="21.000000" value="' . $arr['user_info']['lng'] . '">
+							<label> '.$arr['trans']->Get('LB_LNG').' </label>
+							<input type="text" name="lng" placeholder="'.$arr['trans']->Get('EG').' 21.000000" value="' . $arr['user_info']['lng'] . '">
 						</div>
 						<div class="w-50">
-							<label>Latitude</label>
-							<input type="text" name="lat" placeholder="52.000000" value="' . $arr['user_info']['lat'] . '">
+							<label> '.$arr['trans']->Get('LB_LAT').' </label>
+							<input type="text" name="lat" placeholder="'.$arr['trans']->Get('EG').' 52.000000" value="' . $arr['user_info']['lat'] . '">
 						</div>
 						<div class="w-50">
-							<label>About</label>
-							<textarea name="about" placeholder="Write something">' . $arr['user_info']['about'] . '</textarea>
+							<label>'.$arr['trans']->Get('LB_ABOUT').' </label>
+							<textarea name="about" placeholder="'.$arr['trans']->Get('EG').' Wspaniały opis.">' . $arr['user_info']['about'] . '</textarea>
 						</div>
 						<line></line>
-						<input type="submit" name="aboutinfo" value="Save" class="btn float-right">
+						<input type="submit" name="aboutinfo" value="'.$arr['trans']->Get('SAVE').'" class="btn float-right">
 					</form>
 				</div>
 
 				<div class="box-wrap">
-					<h3>Delivery address</h3>
+					<h3> '.$arr['trans']->Get('PR_DELIVERY').' </h3>
 					<form method="post">
 						<div class="w-50">
-							<label>City</label>
-							<input type="text" name="city" placeholder="Warsaw" value="' . $arr['user_info']['city'] . '">
+							<label> '.$arr['trans']->Get('LB_CITY').' </label>
+							<input type="text" name="city" placeholder="'.$arr['trans']->Get('EG').' Warsaw" value="' . $arr['user_info']['city'] . '">
 						</div>
 						<div class="w-50">
-							<label>Postal</label>
-							<input type="text" name="zip" placeholder="00-000" value="' . $arr['user_info']['zip'] . '">
+							<label> '.$arr['trans']->Get('LB_ZIP').' </label>
+							<input type="text" name="zip" placeholder="'.$arr['trans']->Get('EG').' 00-000" value="' . $arr['user_info']['zip'] . '">
 						</div>
 						<div class="w-50">
-							<label>Address</label>
-							<input type="text" name="address" placeholder="Złota 13/9" value="' . $arr['user_info']['address'] . '">
+							<label> '.$arr['trans']->Get('LB_ADDR').' </label>
+							<input type="text" name="address" placeholder="'.$arr['trans']->Get('EG').' Złota 13/9" value="' . $arr['user_info']['address'] . '">
 						</div>
 						<div class="w-50">
-							<label>Country</label>
-							<input type="text" name="country" placeholder="Polska" value="' . $arr['user_info']['country'] . '">
+							<label> '.$arr['trans']->Get('LB_COUNTRY').' </label>
+							<input type="text" name="country" placeholder="'.$arr['trans']->Get('EG').' Polska" value="' . $arr['user_info']['country'] . '">
 							<!--
 							<select>
 								<option value="Polska">Polska</option>
@@ -267,7 +271,7 @@ class ProfilView extends Component
 							-->
 						</div>
 						<line></line>
-						<input type="submit" name="delivery" value="Save" class="btn float-right">
+						<input type="submit" name="delivery" value="'.$arr['trans']->Get('SAVE').'" class="btn float-right">
 					</form>
 				</div>
 			</div>
@@ -277,17 +281,17 @@ class ProfilView extends Component
 
 	static function Title()
 	{
-		return 'Page title here. Title() method.';
+		return 'Profil';
 	}
 
 	static function Description()
 	{
-		return 'Page description text. Description() method.';
+		return 'Profil settings.';
 	}
 
 	static function Keywords()
 	{
-		return 'Page keywords. Keywords() method';
+		return 'profil, settings';
 	}
 
 	static function Head()
