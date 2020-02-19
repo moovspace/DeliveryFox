b7baff22ccfede15de4f9973119a1722946468af
Marcys <moovspace@github.com>
2020-02-19 19:39:16 +0100

    PhpApix products cart, cart db

--- src/Web/Page/ProductList/ProductBox.php
+++ src/Web/Page/ProductList/ProductBox.php
@@ -6,10 +6,19 @@ class ProductBox
 	static function Show()
 	{
 		// Category
-		$cat = (int) $_GET['category'];
+		$cat = 0;
+		if(!empty($_GET['category'])){
+			$cat = (int) $_GET['category'];
+		}
 		// Pagination
-		$page = (int) $_GET['page'];
-		$perpage = (int) $_GET['perpage'];
+		$page = 1;
+		if(!empty($_GET['page'])){
+			$page = (int) $_GET['page'];
+		}
+		$perpage = 6;
+		if(!empty($_GET['perpage'])){
+			$perpage = (int) $_GET['perpage'];
+		}
 		if($perpage < 1) { $perpage = 6; }
 		if($page < 1) { $page = 1; }
 		$back = $page - 1;
@@ -79,6 +88,42 @@ class ProductBox
 
 				<div class="product-pagine">
 					<a href="?category='.$cat.'&perpage='.$perpage.'&page='.$back.'"> <i class="fas fa-chevron-left"></i> </a> <span id="product-curr-page"> '.$page.' </span> <a href="?category='.$cat.'&perpage='.$perpage.'&page='.$next.'"> <i class="fas fa-chevron-right"></i> </a>
+				</div>
+
+				<div id="add-product-fixed">
+					<div class="top">
+						<img src="/media/img/food-1.jpg">
+						<div class="about">
+							<div class="name"> Pizza hawajska z warzywami</div>
+							<p class="desc">
+							As a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using , making it look like readable English. And a search for will uncover many web sites still in their infancy.
+							</p>
+						</div>
+						<div class="right">
+							<div class="price"> 159.98 <curr>PLN</curr> </div>
+							<div class="add-to-cart-now"> Add to cart </div>
+						</div>
+					</div>
+					<div class="size">
+						<div class="size-btn"> Size 15cm </div>
+						<div class="size-btn"> Size 25cm </div>
+						<div class="size-btn"> Size 35cm </div>
+						<select class="size-btn">
+							<option>Sos łagodny</option>
+						</select>
+					</div>
+					<h3>Addons</h3>
+					<div class="addons">
+						<div class="addon-btn"> Ananas <i class="fas fa-minus"></i> <span class="quantity">1</span> <i class="fas fa-plus"></i> </div>
+						<div class="addon-btn"> Ser kozi <i class="fas fa-minus"></i> <span class="quantity">1</span> <i class="fas fa-plus"></i> </div>
+						<div class="addon-btn"> Szynka <i class="fas fa-minus"></i> <span class="quantity">1</span> <i class="fas fa-plus"></i> </div>
+						<div class="addon-btn"> Ananas <i class="fas fa-minus"></i> <span class="quantity">1</span> <i class="fas fa-plus"></i> </div>
+						<div class="addon-btn"> Ser kozi <i class="fas fa-minus"></i> <span class="quantity">1</span> <i class="fas fa-plus"></i> </div>
+						<div class="addon-btn"> Szynka <i class="fas fa-minus"></i> <span class="quantity">1</span> <i class="fas fa-plus"></i> </div>
+						<div class="addon-btn"> Ananas <i class="fas fa-minus"></i> <span class="quantity">1</span> <i class="fas fa-plus"></i> </div>
+						<div class="addon-btn"> Ser kozi <i class="fas fa-minus"></i> <span class="quantity">1</span> <i class="fas fa-plus"></i> </div>
+						<div class="addon-btn"> Szynka <i class="fas fa-minus"></i> <span class="quantity">1</span> <i class="fas fa-plus"></i> </div>
+					</div>
 				</div>
 			</div>
 		';
