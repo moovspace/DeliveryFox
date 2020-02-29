cd82109e2bcfef3a93f64418d410db67bc0bae13
Marcys <moovspace@github.com>
2020-02-29 14:34:02 +0100

    Delivery homepage

---
+++ src/App/MainPage/BoxThreeIcons.php
@@ -0,0 +1,126 @@
+<?php
+namespace MyApp\App\MainPage;
+
+class BoxThreeIcons
+{
+	static function Html($arr)
+	{
+		$h = '
+			<div class="box3">
+
+				<h1> '.$arr['title'].' </h1>
+				<p> '.$arr['txt'].' </p>
+
+				<div class="center">
+					<div class="box-img">
+						<img src="'.$arr['img1'].'">
+						<h2>'.$arr['txt1'].'</h2>
+						<p>'.$arr['desc1'].'</p>
+					</div>
+
+					<div class="box-img">
+						<img src="'.$arr['img2'].'">
+						<h2>'.$arr['txt2'].'</h2>
+						<p>'.$arr['desc2'].'</p>
+					</div>
+
+					<div class="box-img">
+						<img src="'.$arr['img3'].'">
+						<h2>'.$arr['txt3'].'</h2>
+						<p>'.$arr['desc3'].'</p>
+					</div>
+				</div>
+
+			</div>
+		';
+
+		return $h;
+	}
+
+	static function Style()
+	{
+		$style = '
+		<link href="https://fonts.googleapis.com/css?family=Anton&display=swap&subset=latin-ext" rel="stylesheet">
+
+		<style>
+		.box3{
+			float: left; width: 100%; height: auto; overflow: hidden;
+			text-align: center;
+			background: #e0cda9
+		}
+		.box3 p{
+			float: left; width: 80%; margin-left: 10%;
+		}
+		.box3 .center{
+			float: left; width: 70%; margin-left: 15%;
+			margin-top: 50px;
+			margin-bottom: 50px;
+		}
+		.box3 .center .box-img{
+			display: flex;
+			flex-direction: column;
+			float: left; width: 31.333%;
+			margin: 1%;
+			text-align: center;
+			align-items: center;
+			position: relative;
+		}
+		.box3 .center .box-img img{
+			float: left; width: 50px; height: 50px;
+			z-index: 1; object-fit: cover;
+			filter: brightness(90%);
+		}
+		.box3 h1{
+			margin-top: 60px;
+			font-size: 26px; color: #885342; font-weight: 400;
+			float: left; width: 100%; height: 60px;
+			z-index: 2;
+			font-family: Anton;
+		}
+		.box3 .box-img h2{
+			font-size: 26px; color: #885342; font-weight: 400;
+			float: left; width: 100%;
+			margin-bottom: 0px;
+			z-index: 2;
+			font-family: Anton;
+		}
+		.box3 .center .box-img p{
+			float: left; width: 100%; margin-left: 0%;
+			font-size: 17px; color: #222; font-weight: 500;
+			z-index: 2;
+		}
+
+		.fixed-menu{
+			background: rgba(255,255,255,.01) !important
+		}
+		.fixed-menu .middle .fixed-link{
+			color: #fff;
+		}
+		#cart-btn-show{
+			display: none;
+		}
+
+		@media all and (max-width: 1024px){
+			.box3{
+				display: block;
+			}
+			.box3 .center .box-img{
+				float: left; width: 98%;
+				margin: 30px 1%;
+			}
+			.box3 p{
+				float: left; width: 80%; margin-left: 10%;
+			}
+			.box3 .center .box-img p{
+				float: left; width: 100%; margin-left: 0%;
+			}
+		}
+
+		@media all and (max-width: 480px){
+		}
+		</style>
+		';
+
+		return $style;
+	}
+}
--- src/Web/Home/View.php
+++ src/Web/Home/View.php
@@ -7,6 +7,7 @@ use MyApp\Web\Home\Homepage;
 // Main page
 use MyApp\App\MainPage\TopSlider;
 use MyApp\App\MainPage\BoxFourLinks;
+use MyApp\App\MainPage\BoxThreeIcons;
 
 class View
 {
@@ -20,6 +21,8 @@ class View
 		$h .= TopSlider::Html(['img1' => '/media/home/top-slider-smoked.jpg','txt1' => 'We make and delivers burgers!', 'txt2' => 'Taste some today', 'href1' => '/category', 'title1' => 'Dania', 'href2' => '/gallery', 'title2' => 'Galeria']);
 		// 4 boxes
 		$h .= BoxFourLinks::Html(['img1' => '/media/home/burger.jpg','txt1' => 'Hand crefted burgers!', 'desc1' => 'Taste some today', 'href1' => '/category', 'title1' => 'Nasze dania', 'img2' => '/media/home/pizza.jpg','txt2' => 'Pizza Italiana!', 'desc2' => 'Taste some today', 'href2' => '/category', 'title2' => 'Nasze dania', 'img3' => '/media/home/salad.jpg','txt3' => 'Fresh salads!', 'desc3' => 'Taste some today', 'href3' => '/category', 'title3' => 'Nasze dania', 'img4' => '/media/home/drinks.jpg','txt4' => 'Cold drinks!', 'desc4' => 'Taste some today', 'href4' => '/category', 'title4' => 'Nasze dania']);
+		// 3 Icons order
+		$h .= BoxThreeIcons::Html(['title' => 'How Do I Order?', 'txt' => 'Some text or long description. Some text or long description. Some text or long description.','img1' => '/media/home/pizza-icon.png','txt1' => 'Take Step One', 'desc1' => 'Choose dishes', 'img2' => '/media/home/address-icon.png','txt2' => 'Take Step Two', 'desc2' => 'Enter your Address', 'img3' => '/media/home/plate-icon.png','txt3' => 'And Step Three', 'desc3' => 'Enjoy Your Order', 'href3' => '/category', 'title3' => 'Nasze dania', 'img4' => '/media/home/drinks.jpg']);
 
 		// Show components
 		echo $h;
@@ -30,6 +33,7 @@ class View
 
 		echo TopSlider::Style();
 		echo BoxFourLinks::Style();
+		echo BoxThreeIcons::Style();
 	}
 }
 ?>
